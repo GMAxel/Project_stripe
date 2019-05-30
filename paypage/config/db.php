3e5203021f6bd8ad89cadccbbceffc5ff39a3ad1
GMAxel <axel.tryde.at@gmail.com>
2019-05-30 19:10:25 +0200

    asd

--- paypage/config/db.php
+++
@@ -1,7 +0,0 @@
-<?php
-    // DB Params
-    define("DB_HOST", "localhost");
-    define("DB_USER", "root");
-    define("DB_PASS", "root");
-    define("DB_NAME", "stripe_db");
-    define("DB_CHARSET", "utf8mb4");
